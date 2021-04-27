7df5e8946e14e9fb6e4d71a7ac7a64001b4e3301
DanielAwaritefe <awaritefe@gmail.com>
2021-04-27 17:51:38 +0100

    app and searchbar component made with onchange property on search

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" onChange={this.onInputChange} />
+                        {/* alternative for above if single line. Can remove the function above with this */}
+                        {/* <input type="text" onChange={(event) => console.log(event.target.value) } /> */}
+                    </div>
+                </form>
+            </div>
+        );
+    };
+};
+export default SearchBar;
